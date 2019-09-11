342ea6c4496460098612ef49c9b8c7ea9d086a85
Pratik Karki <predatoramigo@gmail.com>
2019-09-11 21:56:26 +0545

    Update the structure of repository so that src is used

--- pytest_workout/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Top-level package for pytest-workout."""
-
-from .api import (Task, TasksException, add, get, list_tasks, count, update,
-                  delete, delete_all, unique_id, start_tasks_db, stop_tasks_db)
-
-__author__ = """Pratik Karki"""
-__email__ = 'predatoramigo@gmail.com'
-__version__ = '0.0.1'
